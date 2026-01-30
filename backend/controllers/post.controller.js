6f2d770a74ad0f6d75455ae98c240e25375f2b3b
fuaddot <fuadarage10@gmail.com>
2026-01-30 08:25:00 +0300

    1-update route file and path

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,5 @@
 import Notification from "../models/notification.model.js";
 import post from "../models/post.model.js";
-import post from "../models/post.model.js";
 import User from "../models/user.model.js";
 import { v2 as cloudinary } from "cloudinary";
 
